e0d8909f1a0d6441db6fb1230228cabe7d781a0d
asadev1411 <kaisar1990@mail.ru>
2022-12-16 16:45:03 +0500

    Completed HomeWork №10

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,25 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        //Task №1
+        System.out.println("Задание №1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName + ".");
 
+        //Task №2
+        System.out.println(" ");
+        System.out.println("Задание №2");
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUpper + ".");
+
+        //Task №3
+        System.out.println(" ");
+        System.out.println("Задание №3");
+        String fullNameSemen = "Иванов Семён Семёнович";
+        fullNameSemen = fullNameSemen.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullNameSemen + ".");
     }
 }
